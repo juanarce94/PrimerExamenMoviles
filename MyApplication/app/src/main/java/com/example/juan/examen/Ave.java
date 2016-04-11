f52fc858b1d51924b6a5fb44343bcde216562ccd
juanmiguelarce94 <juanmiguelarce94@gmail.com>
2016-04-10 18:05:12 -0600

    added methods insert, update and delete to de sqlite database

---
+++ MyApplication/app/src/main/java/com/example/juan/examen/Ave.java
@@ -0,0 +1,19 @@
+package com.example.juan.examen;
+
+/**
+ * Created by Juan on 4/10/2016.
+ */
+public class Ave {
+
+    public static String nombreComun = "";
+    public static String nombreCientifico = "";
+    public static String descripcion = "";
+    public static String generalidades = "";
+
+    public Ave(String nombreComun,String nombreCientifico,String descripcion,String generalidades) {
+        this.nombreComun = nombreComun;
+        this.nombreCientifico = nombreCientifico;
+        this.descripcion = descripcion;
+        this.generalidades = generalidades;
+    }
+}
