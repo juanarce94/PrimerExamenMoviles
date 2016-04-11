bb047dc5dd3043529ae05e95f3f8f8a58c560f5d
juanmiguelarce94 <juanmiguelarce94@gmail.com>
2016-04-11 12:04:26 -0600

    idk

--- MyApplication/app/src/main/java/com/example/juan/examen/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/juan/examen/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DataBaseManager dbManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DataBaseManager adbManager = new DataBaseManager(this, "EXAMEN_DATA_BASE");
-        //UsersDataBaseManager udbManager = new UsersDataBaseManager(this,"users");
-
+        dbManager = new DataBaseManager(this, "EXAMEN_DATA_BASE");
     }
+
+    private void addDefaultUsersToDataBase(){
+        for(int i = 0; i < 6; i++){
+            User newUser = new User("name"+String.valueOf(i),"pass"+String.valueOf(i));
+            dbManager.insertDB(newUser, DataBaseManager.USERS_TABLE_NAME);
+        }
+    }
+
+
 }
